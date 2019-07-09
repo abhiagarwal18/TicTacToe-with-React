10e34f1954a1c5ca6acba644e6fc14c263057283
abhiagarwal18 <itsagarwal18@gmail.com>
2019-07-09 18:40:29 +0530

    Restart functionality to be added

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MyFirstComp from './MyFirstComp'
 
-ReactDOM.render(MyFirstComp />, document.getElementById('root'));
+ReactDOM.render(<MyFirstComp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
